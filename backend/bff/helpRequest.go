efc47ba818171f64af591a526d2ca08803acc7cb
Daniel Randall <dgrandall@me.com>
2019-12-19 14:43:26 -0700

    added help request backend stuff

---
+++ backend/bff/helpRequest.go
@@ -0,0 +1,52 @@
+package bff
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/byuoitav/common/v2/events"
+	"go.uber.org/zap"
+)
+
+type HelpRequest struct {
+}
+
+type HelpRequestMessage struct {
+	Message string `json:"msg"`
+}
+
+func (hr HelpRequest) Do(c *Client, data []byte) {
+	var msg HelpRequestMessage
+
+	if err := json.Unmarshal(data, &msg); err != nil {
+		c.Out <- ErrorMessage(fmt.Errorf("invalid value for helpRequest: %s", err))
+		return
+	}
+
+	c.Info("Sending help request", zap.String("helpMsg", msg.Message))
+
+	c.SendEvent <- events.Event{
+		GeneratingSystem: os.Getenv("SYSTEM_ID"),
+		User:             c.id,
+		Key:              "help-request",
+		Value:            "confirm",
+		Data:             msg.Message,
+		Timestamp:        time.Now(),
+		EventTags: []string{
+			events.Alert,
+		},
+		AffectedRoom: events.BasicRoomInfo{
+			BuildingID: c.buildingID,
+			RoomID:     c.roomID,
+		},
+		TargetDevice: events.BasicDeviceInfo{
+			BasicRoomInfo: events.BasicRoomInfo{
+				BuildingID: c.buildingID,
+				RoomID:     c.roomID,
+			},
+			DeviceID: c.selectedControlGroupID,
+		},
+	}
+}
--- backend/bff/uiconfig.go
+++ backend/bff/uiconfig.go
@@ -31,10 +31,11 @@ type Preset struct {
 
 	ActionsConfig json.RawMessage `json:"actions,omitempty"`
 	Actions       struct {
-		SetInput  SetInput  `json:"setInput,omitempty"`
-		SetVolume SetVolume `json:"setVolume,omitempty"`
-		SetMuted  SetMuted  `json:"setMuted,omitempty"`
-		SetPower  SetPower  `json:"setPower,omitempty"`
+		SetInput    SetInput    `json:"setInput,omitempty"`
+		SetVolume   SetVolume   `json:"setVolume,omitempty"`
+		SetMuted    SetMuted    `json:"setMuted,omitempty"`
+		SetPower    SetPower    `json:"setPower,omitempty"`
+		HelpRequest HelpRequest `json:"helpRequest,omitempty"`
 	} `json:"-"`
 }
 
